5619ac5a9788466c62acd008ef488414f4771d33
JessicaGreben <jessica.greben1+github@gmail.com>
2018-09-16 17:25:22 -0700

    add init solution

--- challenge-1/golang-challenge-1-drum_machine/decoder.go
+++ challenge-1/golang-challenge-1-drum_machine/decoder.go
@@ -1,15 +1,46 @@
 package drum
 
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+type Pattern struct {
+	Header string
+	Tempo  string
+	Tracks string
+}
+
+func (p Pattern) String() string {
+	return fmt.Sprintf("%s%s%s",
+		p.Header,
+		p.Tempo,
+		p.Tracks,
+	)
+}
+
 // DecodeFile decodes the drum machine file found at the provided path
 // and returns a pointer to a parsed pattern which is the entry point to the
 // rest of the data.
-// TODO: implement
 func DecodeFile(path string) (*Pattern, error) {
-	p := Pattern{}
+	fd, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("os.Read failed for file: %s. Error: %v", path, err)
+	}
+	defer fd.Close()
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		fmt.Print("os.Stat failed: ", err)
+	}
+	size := fi.Size()
+	fileBytes := readNextBytes(fd, int(size))
+	buffer := bytes.NewBuffer(fileBytes)
+
+	header, tempo := decodeHeader(buffer)
+	allTracks := decodeTracks(buffer)
+
+	p := Pattern{header, tempo, allTracks}
 	return &p, nil
 }
-
-// Pattern is the high level representation of the
-// drum pattern contained in a .splice file.
-// TODO: implement
-type Pattern struct{}
--- challenge-1/golang-challenge-1-drum_machine/drum.go
+++ challenge-1/golang-challenge-1-drum_machine/drum.go
@@ -1,7 +1,128 @@
 // Package drum is supposed to implement the decoding of .splice drum machine files.
 // See golang-challenge.com/go-challenge1/ for more information
-
-// write a binary decoder
 package drum
 
-func DecodeFile(path string) (*Pattern, error)
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+type Header struct {
+	_       [14]byte
+	Version [32]byte
+}
+
+func (h Header) String() string {
+	// convert to byte type in order to trim padding
+	version := h.Version[:]
+	return fmt.Sprintf("Saved with HW Version: %s\n",
+		bytes.Trim(version, "\x00"),
+	)
+}
+
+type Tempo struct {
+	Tempo float32
+}
+
+func (t Tempo) String() string {
+	tempo := strings.TrimSuffix(fmt.Sprintf("%.1f", t.Tempo), ".0")
+	return fmt.Sprintf("Tempo: %s\n", tempo)
+}
+
+type TrackHeader struct {
+	Id         uint8
+	NameLength uint32
+}
+
+type Steps struct {
+	Steps [16]byte
+}
+
+type Track struct {
+	Id    uint8
+	Name  string
+	Steps string
+}
+
+func (t Track) String() string {
+	return fmt.Sprintf("(%d) %s\t|%s|%s|%s|%s|\n",
+		t.Id,
+		string(t.Name),
+		t.Steps[0:4],
+		t.Steps[4:8],
+		t.Steps[8:12],
+		t.Steps[12:16],
+	)
+}
+
+func readNextBytes(file *os.File, number int) []byte {
+	bytes := make([]byte, number)
+	_, err := file.Read(bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bytes
+}
+
+func decodeHeader(buffer *bytes.Buffer) (string, string) {
+	h := Header{}
+	err := binary.Read(buffer, binary.BigEndian, &h)
+	if err != nil {
+		fmt.Print("Header: binary.Read failed ", err)
+	}
+
+	t := Tempo{}
+	err = binary.Read(buffer, binary.LittleEndian, &t)
+	if err != nil {
+		fmt.Print("Tempo: binary.Read failed ", err)
+	}
+	return fmt.Sprint(h), fmt.Sprint(t)
+}
+
+func decodeTracks(buffer *bytes.Buffer) string {
+	allTracks := ""
+	for {
+		trackHeader := TrackHeader{}
+		err := binary.Read(buffer, binary.BigEndian, &trackHeader)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Print("Tracks: binary.Read failed:", err)
+		}
+
+		trackName := make([]byte, trackHeader.NameLength)
+		buffer.Read(trackName)
+
+		s := Steps{}
+		err = binary.Read(buffer, binary.BigEndian, &s)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Print("Steps: binary.Read failed:", err)
+		}
+
+		beats := fmtBeats(s.Steps)
+		track := Track{trackHeader.Id, string(trackName), beats}
+		allTracks = fmt.Sprintf(`%s%s`, allTracks, track)
+	}
+	return allTracks
+}
+
+func fmtBeats(steps [16]byte) string {
+	beats := ""
+	for i := 0; i < 16; i++ {
+		if steps[i] == 1 {
+			beats += "x"
+		} else {
+			beats += "-"
+		}
+	}
+	return beats
+}
